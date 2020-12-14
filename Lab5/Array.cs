bde6e98fb7a57742f687a4aec5ede4095e1bcf39
daniil789 <pechenkin031199@mail.ru>
2020-12-14 22:31:31 +0500

    Add project files.

---
+++ Lab5/Array.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab5
+{
+    class Array
+    {
+        int[] _array;
+
+        public Array(int i)
+        {
+            _array = new int[i];
+        }
+
+        public void AddElement(int k)
+        {
+            int[] newArray = new int[_array.Length + k];
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                newArray[i] = _array[i];
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                int newElement = Convert.ToInt32(Console.ReadLine());
+                newArray[_array.Length + i] = newElement;
+            }
+
+            _array = newArray;
+
+        }
+
+        public void FillArray()
+        {
+            Console.WriteLine("Заполните массив");
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                int newElement = Input();
+                _array[i] = newElement;
+            }
+        }
+
+        public void FillArrayRandomElemetns()
+        {
+            var random = new Random();
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = random.Next(20);
+            }
+        }
+
+        public void PrintArray()
+        {
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                Console.WriteLine($"{i} - {_array[i]}");
+            }
+        }
+
+        private int Input()
+        {
+            while (true)
+            {
+                try
+                {
+                    int newElement = Convert.ToInt32(Console.ReadLine());
+                    return newElement;
+                }
+                catch
+                {
+                    Error();
+                    continue;
+                }
+            }
+        }
+
+        private void Error()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error");
+            Console.ResetColor();
+
+        }
+    }
+}
---
+++ Lab5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        static Array _array;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Работа с одномерными массивами");
+
+                var action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "1":
+                        ActionsWithArray();
+                        break;
+                }
+            }
+        }
+
+        public static void ActionsWithArray()
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Создать массив 2. Заполнить массив вручную 3. Заполнить массив случайными числами 4. Добавить элементы 5. Напечатать массив 6. Назад");
+
+                var action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "1":
+                        _array = CreateArray();
+                        break;
+                    case "2":
+                        _array.FillArray();
+                        break;
+                    case "3":
+                        _array.FillArrayRandomElemetns();
+                        break;
+                    case "4":
+                        _array.AddElement(5);
+                        break;
+                    case "5":
+                        _array.PrintArray();
+                        break;
+                    case "6":
+                        return;
+                       
+                }
+            }
+        }
+
+        public static Array CreateArray()
+        {
+
+            Console.WriteLine("Введите размерность массива");
+            int arrayLenght = Convert.ToInt32(Console.ReadLine());
+            var array = new Array(arrayLenght);
+
+            return array;
+        }
+    }
+}
